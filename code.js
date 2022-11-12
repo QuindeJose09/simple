aeb101029f97a0eac1f3b2dc0a10a97c9e3b3a16
QuindeJose09 <quindejose775gmail.com>
2022-11-11 19:50:06 -0500

    code and styles

---
+++ code.js
@@ -0,0 +1,21 @@
+
+// const insertHtml = () => {
+//   let container = document.getElementById('container')
+  
+
+//   for (let i = 0; i < 6; i++) {
+//     console.log("object")
+//     container.innerHTML += 
+//     (`<div class="item item-forma swiper-slide swiper-slide-duplicate-next">
+//         <div class="item__box">
+//           <img src="./svg/logo.svg" alt=""> 
+//         </div>
+//         <div class="item__info">
+//           <p><b>Item ${i+1}</b></p>
+//           <p>Lorem ipsum dolor sit.</p>
+//         </div>
+//       </div>`)
+//   }
+// };
+
+// insertHtml()
